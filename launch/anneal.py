607bdef05d050f2620e76478024d61e75e084d52
Ishaan Watts <wattsishaan18@gmail.com>
2026-01-25 05:56:21 -0500

    auto anneal tokens

--- launch/anneal.py
+++ launch/anneal.py
@@ -3,6 +3,23 @@
 from experiments import ArtifactSet  # type: ignore
 from launch.artifacts import AnnealedModel, ModelEvaluation
 
+PT_TOKENS = {
+    20: {
+        17: {3e-3: [4, 8, 16]},
+        65: {1e-3: [32, 64]},
+    },
+    60: {
+        25 : {1e-3: [12, 24]},
+        100: {6e-4: [48, 96]},
+        200: {3e-4: [192]},
+    },
+    150: {
+        31: {1e-3: [15, 30]},
+        61: {6e-4: [60]},
+        121: {3e-4: [120]},
+    },
+}
+
 def build_annealed_models(pretrained_models: ArtifactSet) -> ArtifactSet:
     return pretrained_models.map_flatten(
         lambda pretrained_model: ArtifactSet.from_product(
@@ -13,7 +30,7 @@ def build_annealed_models(pretrained_models: ArtifactSet) -> ArtifactSet:
                 anneal_optim=["adamw", "sam"],
                 anneal_match="token",
                 anneal_percent=[10],
-                pt_token=[48, 96]
+                pt_token=PT_TOKENS[pretrained_model.model_size][pretrained_model.train_tokens][pretrained_model.pt_lr]
             )
         )
     )
